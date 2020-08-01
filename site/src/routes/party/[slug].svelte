c89c960f667514d48d68f030a9218be2d10ecda1
Edd Hannay <edd@vega.xyz>
2020-08-01 16:48:28 +0100

    Refactor party order list

--- site/src/routes/party/[slug].svelte
+++ site/src/routes/party/[slug].svelte
@@ -5,6 +5,8 @@
   import MarketLink from "../../components/MarketLink.svelte";
   import {Parties} from "../../stores/parties.js";
   import Hash from "../../components/Hash.svelte";
+  import BlockLink from "../../components/BlockLink.svelte";
+  import OrderLink from "../../components/OrderLink.svelte";
 
   const {page} = stores();
   let {slug} = $page.params;
@@ -50,44 +52,25 @@
       {/each}
     </ul>
     <h2>Recent orders</h2>
+    <ul>
     {#each party.orders as or}
+      <li>
+        <OrderLink id={or.id} short={true} />:
         {#if or.remaining === 0}
-          &nbsp;
-      &nbsp;&nbsp;<s>{or.side} in <MarketLink id={or.market.id} /></s>: {or.size} @ {or.price}
-        in <a href="{`/blocks/${getBlockFromTradeId(or.id)}`}">{getBlockFromTradeId(or.id)}</a><br>
+        <s>{or.side} in <MarketLink id={or.market.id} /></s>: {or.size} @ {or.price}
         {:else if (or.remaining === or.size)}
-          &nbsp;
-          &nbsp;&nbsp;<b>{or.side} in <MarketLink id={or.market.id} /></b>: {or.size} @ {or.price}
-          in <a href="{`/blocks/${getBlockFromTradeId(or.id)}`}">{getBlockFromTradeId(or.id)}</a><br>
+        <b>{or.side} in <MarketLink id={or.market.id} /></b>: {or.size} @ {or.price}
         {:else}
-          <details>
-          <summary>
           {#if or.remaining !== '0'}
             <b>{or.side} in <MarketLink id={or.market.id} /></b>: {or.size} @ {or.price} <i>(remaining: {or.remaining})</i>
-            in <a href="{`/blocks/${getBlockFromTradeId(or.id)}`}">{getBlockFromTradeId(or.id)}</a>
           {:else}
             <s>{or.side} in <MarketLink id={or.market.id} /></s>: {or.size} @ {or.price} (filled)
-            in <a href="{`/blocks/${getBlockFromTradeId(or.id)}`}">{getBlockFromTradeId(or.id)}</a>
           {/if}
-          </summary>
-          {#if or.trades && or.trades.length !== 0}
-            <div style="margin-left: 60px">
-            <h3>Trades</h3>
-            {#each or.trades as t}
-              <li>
-                {#if or.side === 'Buy'}
-                <b>{t.size} -> <a on:click="{() => {navigate(t.seller.id)}}" href='/party/{t.seller.id}'>{t.seller.id}</a></b>
-                {:else}
-                <b>{t.size} -> <a on:click="{() => {navigate(t.buyer.id)}}" href='/party/{t.buyer.id}'>{t.buyer.id}</a></b>
-                {/if}
-                in <a href="{`/blocks/${getBlockFromTradeId(t.id)}`}">{getBlockFromTradeId(t.id)}</a>
-              </li>
-            {/each}
-            </div>
-            {/if}
-          </details>
         {/if}
+        in <BlockLink id={or.id} />
+      </li>
     {/each}
+    </ul>
   {:else}
     <h1>Loading...</h1>
   {/if}
