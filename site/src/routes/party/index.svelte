2677dbf137db6806b3ab951ea212c952926eb88f
Edd Hannay <edd@vega.xyz>
2020-10-14 12:29:26 +0100

    Remove leaderboard stuff

--- site/src/routes/party/index.svelte
+++ site/src/routes/party/index.svelte
@@ -1,53 +1,8 @@
 <script>
-  import {store} from "../../stores/leaderboard.js";
-
   import {onDestroy, onMount} from 'svelte'
   import PartyLink from "../../components/PartyLink.svelte";
-
-  let unsubscribe
-  let leaderboard
-
-  onDestroy(() => {
-    if (unsubscribe) {
-      unsubscribe();
-      unsubscribe = null;
-    }
-  });
-
-  function updateLeaderboard(data) {
-    leaderboard = data.leaderboard;
-  }
-
-  onMount(() => {
-    if (!unsubscribe) {
-      unsubscribe = store.subscribe(updateLeaderboard);
-    }
-  })
-
 </script>
 
 <div>
-  <h1>Trader Leaderboard</h1>
-  {#if leaderboard}
-    <table>
-      <thead>
-      <tr>
-        <th>Public Key</th>
-        <th>Total Balance</th>
-      </tr>
-    </thead>
-    {#each [...leaderboard] as [ id, { trader } ]}
-      <tr>
-        <td>
-          <PartyLink id={trader.publicKey} />
-        </td>
-        <td>
-          ${trader.totalUsdVal}
-        </td>
-      </tr>
-    {/each}
-    </table>
-  {:else}
-      This will work when testnet ports are open...
-  {/if}
+  <p>Select a party from a block or trading view to see their details</p>
 </div>
